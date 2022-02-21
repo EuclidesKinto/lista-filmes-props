8b7b133dc8227d435356cf3c3ebc63508b2bc36f
Euclides <euclideskinto@gmail.com>
2022-02-21 11:50:45 -0300

    passando dados para props dinamicamente

--- src/components/FilmesLista.vue
+++ src/components/FilmesLista.vue
@@ -3,28 +3,19 @@
 
     <!-- coluna 1 -->
     <div class="col-8">
-
       <h2>Filmes</h2>
-
       <div class="list-group list-group-flush">
-        <FilmesListaIten filmeTitulo="Vingadores: Guerra Infinita"/>
-        <FilmesListaIten filmeTitulo="Vingadores: 2"/>
-        <FilmesListaIten filmeTitulo="Pentera Negra"/>
+        <FilmesListaIten v-for="(filme, i) in filmes" :key="i" :filmeTitulo="filme"/>
       </div>
     </div>
-
     <!-- coluna 2 -->
     <div class="col-4">
-
       <FilmesListaItenInfo/>
-
     </div>
-
   </div>
 </template>
 
 <script>
-
 import FilmesListaIten from './FilmesListaIten.vue'
 import FilmesListaItenInfo from './FilmesListaItenInfo.vue'
 
@@ -32,6 +23,16 @@ export default {
   components: {
     FilmesListaIten,
     FilmesListaItenInfo
-  }
+  },
+  data() {
+    return {
+      filmes:[
+        'Vingadores: Guerra Infinita 1',
+        'Vingadores: Guerra Infinita 2',
+        'Vingadores: Guerra Infinita 3',
+
+      ]
+    }
+  },
 }
 </script>
